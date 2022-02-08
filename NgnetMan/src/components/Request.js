0c89f868be111d114b6dc36538ebe3e59d0d8b71
Konstantin Velev <ABZQNZZ@mmm.com>
2022-02-08 16:51:45 +0200

    Return unfinished changes

--- NgnetMan/src/components/Request.js
+++ NgnetMan/src/components/Request.js
@@ -10,7 +10,6 @@ export function Request() {
     const bodyParams = common.params;
 
     let [action, setAction] = useState();
-    let [address, setAddress] = useState(undefined);
     let { setResponse } = useContext(HttpContext);
 
     function sendRequest(e) {
@@ -21,23 +20,8 @@ export function Request() {
         if (common.params[action]) {
             data = common.params[action].reduce((acc, curr) => {
                 let key = curr.value;
-                if (key == 'age') {
-                    let value = formData.get(curr.value);
-                    if (value === "") {
-                        value = 0;
-                    }
-                    else {
-                        value = parseInt(value);
-                    }
-                    acc[key] = value
-                    return acc;
-                }
-                else if (key == 'address') {
-                    acc[key] = formData.get(curr.value);
-                    return acc;
-                }
-
-                acc[key] = formData.get(curr.value);
+                const value = formData.get(curr.value);
+                acc[key] = value ? value : null;
                 return acc;
             }, {});
         }
@@ -49,23 +33,6 @@ export function Request() {
 
     function getAction(e) {
         setAction(e.value);
-    }
-
-    function toggleAddressHandler(param) {
-        if(param != undefined ){
-           
-                setAddress(
-                    param.nasted?.map((param) => (
-                        <>
-                            <label>{param.label}</label>
-                            <input type='text' className='nasted-input' name={param.value} />
-                        </>
-                    ))
-                );
-        }
-        else {
-            setAddress(undefined);
-        }
     }
 
     return (
@@ -80,14 +47,7 @@ export function Request() {
                     {common.params[action]?.map((param) => (
                         <div className="param" key={param.value}>
                             <label>{param.label}</label>
-                            {
-                                param.value === 'address'
-                                    ? <>
-                                        <input type='text' onClick={toggleAddressHandler(param)}></input>
-                                        {address}
-                                    </>
-                                    : <input type={param.value === 'age' ? 'number' : 'text'} name={param.value} />
-                            }
+                            <input name={param.value} />
                         </div>
                     ))}
                 </div>
